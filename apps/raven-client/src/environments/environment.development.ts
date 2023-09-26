aa7477475f4a1aba25bd5e785c70b8103cda821b
FilipMakowski <102666802+FilipMakowski@users.noreply.github.com>
2023-09-26 13:51:10 +0200

    Updated API URL for DEV

--- apps/raven-client/src/environments/environment.development.ts
+++ apps/raven-client/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://as-wa-mub-raven-dev.azurewebsites.net',
+  apiUrl: 'https://raven.test.mubadalacapital.ae',
   adClientId: '88c53dfa-5389-4ee1-a126-0ed17d3a7db4',
   adAuthority:
     'https://login.microsoftonline.com/9e89d976-bede-4fef-88b6-5d6b2aea2e02',
