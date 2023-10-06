2c29cf2748d8620deeb3681fe0453d68d271d591
Mateusz Kłosiński <mkklosin@gmail.com>
2023-10-06 13:49:07 +0200

    RAVE-43 | Updated unit test

--- libs/rvnc-opportunities/src/lib/services/opportunities.service.spec.ts
+++ libs/rvnc-opportunities/src/lib/services/opportunities.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClientModule } from '@angular/common/http';
 import { OpportunitiesService } from './opportunities.service';
 
 describe('OpportunitiesService', () => {
   let service: OpportunitiesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(OpportunitiesService);
   });
 
