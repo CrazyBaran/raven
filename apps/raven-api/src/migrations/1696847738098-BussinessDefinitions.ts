f14bfe689e23cab561c586f989f64e335698a8ce
Szymon Dziedzic <szymon.d@curvestone.io>
2023-10-09 12:36:37 +0200

    RAVE-84 regenerated migrations

--- apps/raven-api/src/migrations/1696847738098-BussinessDefinitions.ts
+++ apps/raven-api/src/migrations/1696847738098-BussinessDefinitions.ts
@@ -1,15 +1,9 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class BussinessStructure1696508396234 implements MigrationInterface {
-  public name = 'BussinessStructure1696508396234';
+export class BussinessDefinitions1696847738098 implements MigrationInterface {
+  public name = 'BussinessDefinitions1696847738098';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `CREATE TABLE "rvn_affinity_organisations" ("id" uniqueidentifier NOT NULL CONSTRAINT "DF_aef5ea4009042fc897576b01eed" DEFAULT NEWSEQUENTIALID(), "name" nvarchar(255) NOT NULL, "internal_id" int NOT NULL, CONSTRAINT "PK_aef5ea4009042fc897576b01eed" PRIMARY KEY ("id"))`,
-    );
-    await queryRunner.query(
-      `CREATE UNIQUE INDEX "IDX_aef5ea4009042fc897576b01ee" ON "rvn_affinity_organisations" ("id") `,
-    );
     await queryRunner.query(
       `CREATE TABLE "rvn_note_fields" ("id" uniqueidentifier NOT NULL CONSTRAINT "DF_09993575d11ca0f7734dc5af714" DEFAULT NEWSEQUENTIALID(), "name" nvarchar(50) NOT NULL, "type" nvarchar(50) NOT NULL, "order" int NOT NULL, "value" nvarchar(255), "note_group_id" uniqueidentifier NOT NULL, "created_by_id" uniqueidentifier NOT NULL, "created_at" datetime2 NOT NULL CONSTRAINT "DF_33525b7287435d31d3dcd66900f" DEFAULT getdate(), "updated_at" datetime2 NOT NULL CONSTRAINT "DF_f476ca415108be0c9831f507716" DEFAULT getdate(), "updated_by_id" uniqueidentifier NOT NULL, CONSTRAINT "PK_09993575d11ca0f7734dc5af714" PRIMARY KEY ("id"))`,
     );
@@ -26,13 +20,25 @@ export class BussinessStructure1696508396234 implements MigrationInterface {
       `CREATE UNIQUE INDEX "IDX_580829bcf208c4d510ec4c0150" ON "rvn_note_fields" ("id", "note_group_id") `,
     );
     await queryRunner.query(
+      `CREATE TABLE "rvn_pipeline_stages" ("id" uniqueidentifier NOT NULL CONSTRAINT "DF_b6fa3f566faad372719dd028e4b" DEFAULT NEWSEQUENTIALID(), "pipeline_definition_id" uniqueidentifier NOT NULL, "display_name" nvarchar(255) NOT NULL, "mapped_from" nvarchar(255) NOT NULL, "order" int NOT NULL, CONSTRAINT "PK_b6fa3f566faad372719dd028e4b" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_b6fa3f566faad372719dd028e4" ON "rvn_pipeline_stages" ("id") `,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "rvn_pipeline_definitions" ("id" uniqueidentifier NOT NULL CONSTRAINT "DF_d8f3d7bcca95f68d0668cfdee32" DEFAULT NEWSEQUENTIALID(), "name" nvarchar(255) NOT NULL, "is_default" bit NOT NULL CONSTRAINT "DF_37e732d84f3c1da7cb76a3a8a8c" DEFAULT 0, CONSTRAINT "PK_d8f3d7bcca95f68d0668cfdee32" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_eb4435b0a20e30a14eeb869e6a" ON "rvn_pipeline_definitions" ("id", "name") `,
+    );
+    await queryRunner.query(
       `CREATE TABLE "rvn_organisations" ("id" uniqueidentifier NOT NULL CONSTRAINT "DF_0752f17d26b95923198296b8ffb" DEFAULT NEWSEQUENTIALID(), "name" nvarchar(255) NOT NULL, "domains" ntext NOT NULL, CONSTRAINT "PK_0752f17d26b95923198296b8ffb" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `CREATE UNIQUE INDEX "IDX_0752f17d26b95923198296b8ff" ON "rvn_organisations" ("id") `,
     );
     await queryRunner.query(
-      `CREATE TABLE "rvn_opportunities" ("id" uniqueidentifier NOT NULL CONSTRAINT "DF_6194e07c23b849fdaef2763ea3c" DEFAULT NEWSEQUENTIALID(), "organisation_id" nvarchar(255) NOT NULL, CONSTRAINT "PK_6194e07c23b849fdaef2763ea3c" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "rvn_opportunities" ("id" uniqueidentifier NOT NULL CONSTRAINT "DF_6194e07c23b849fdaef2763ea3c" DEFAULT NEWSEQUENTIALID(), "organisation_id" uniqueidentifier NOT NULL, "pipeline_definition_id" uniqueidentifier NOT NULL, "pipeline_stage_id" uniqueidentifier NOT NULL, CONSTRAINT "PK_6194e07c23b849fdaef2763ea3c" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `CREATE UNIQUE INDEX "IDX_6194e07c23b849fdaef2763ea3" ON "rvn_opportunities" ("id") `,
@@ -112,6 +118,32 @@ export class BussinessStructure1696508396234 implements MigrationInterface {
     await queryRunner.query(
       `CREATE UNIQUE INDEX "IDX_2aaec7693a56e203e22aab677a" ON "rvn_field_definitions" ("id", "group_id") `,
     );
+    await queryRunner.query(`ALTER TABLE "rvn_teams" DROP COLUMN "domain"`);
+    await queryRunner.query(
+      `ALTER TABLE "rvn_teams" DROP COLUMN "saml_issuer"`,
+    );
+    await queryRunner.query(`ALTER TABLE "rvn_teams" DROP COLUMN "saml_cert"`);
+    await queryRunner.query(
+      `ALTER TABLE "rvn_teams" DROP COLUMN "saml_entry_point"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_teams" DROP CONSTRAINT "DF_3cc5c2be448344d969685905f1d"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_teams" DROP COLUMN "saml_sign_assertions"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_teams" DROP CONSTRAINT "DF_c8bb9137ca0723c459be900c543"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_teams" DROP COLUMN "saml_sign_authn_response"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_teams" DROP CONSTRAINT "DF_378512aa10f5fa91d39052f2264"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_teams" DROP COLUMN "saml_identifier_format"`,
+    );
     await queryRunner.query(
       `ALTER TABLE "rvn_note_fields" ADD CONSTRAINT "FK_c9e70fcbc2db671965a32e428fc" FOREIGN KEY ("note_group_id") REFERENCES "rvn_note_field_groups"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
     );
@@ -122,7 +154,16 @@ export class BussinessStructure1696508396234 implements MigrationInterface {
       `ALTER TABLE "rvn_note_fields" ADD CONSTRAINT "FK_d8ea8d6c5643a6688ff06d40078" FOREIGN KEY ("updated_by_id") REFERENCES "rvn_users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
     await queryRunner.query(
-      `ALTER TABLE "rvn_opportunities" ADD CONSTRAINT "FK_6194e07c23b849fdaef2763ea3c" FOREIGN KEY ("id") REFERENCES "rvn_organisations"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+      `ALTER TABLE "rvn_pipeline_stages" ADD CONSTRAINT "FK_cc194b75a8412013552d8350452" FOREIGN KEY ("pipeline_definition_id") REFERENCES "rvn_pipeline_definitions"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_opportunities" ADD CONSTRAINT "FK_edb7a5ee96de9c42e7341d67026" FOREIGN KEY ("organisation_id") REFERENCES "rvn_organisations"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_opportunities" ADD CONSTRAINT "FK_874609f835593c95675cbe22aef" FOREIGN KEY ("pipeline_definition_id") REFERENCES "rvn_pipeline_definitions"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_opportunities" ADD CONSTRAINT "FK_fd3f37a1e82ffe337ee6601218b" FOREIGN KEY ("pipeline_stage_id") REFERENCES "rvn_pipeline_stages"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
     await queryRunner.query(
       `ALTER TABLE "rvn_notes" ADD CONSTRAINT "FK_f0037ab29541227112fec724e1f" FOREIGN KEY ("previous_version_id") REFERENCES "rvn_notes"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
@@ -206,7 +247,16 @@ export class BussinessStructure1696508396234 implements MigrationInterface {
       `ALTER TABLE "rvn_notes" DROP CONSTRAINT "FK_f0037ab29541227112fec724e1f"`,
     );
     await queryRunner.query(
-      `ALTER TABLE "rvn_opportunities" DROP CONSTRAINT "FK_6194e07c23b849fdaef2763ea3c"`,
+      `ALTER TABLE "rvn_opportunities" DROP CONSTRAINT "FK_fd3f37a1e82ffe337ee6601218b"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_opportunities" DROP CONSTRAINT "FK_874609f835593c95675cbe22aef"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_opportunities" DROP CONSTRAINT "FK_edb7a5ee96de9c42e7341d67026"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_pipeline_stages" DROP CONSTRAINT "FK_cc194b75a8412013552d8350452"`,
     );
     await queryRunner.query(
       `ALTER TABLE "rvn_note_fields" DROP CONSTRAINT "FK_d8ea8d6c5643a6688ff06d40078"`,
@@ -216,6 +266,36 @@ export class BussinessStructure1696508396234 implements MigrationInterface {
     );
     await queryRunner.query(
       `ALTER TABLE "rvn_note_fields" DROP CONSTRAINT "FK_c9e70fcbc2db671965a32e428fc"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_teams" ADD "saml_identifier_format" nvarchar(255) NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_teams" ADD CONSTRAINT "DF_378512aa10f5fa91d39052f2264" DEFAULT 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress' FOR "saml_identifier_format"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_teams" ADD "saml_sign_authn_response" bit NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_teams" ADD CONSTRAINT "DF_c8bb9137ca0723c459be900c543" DEFAULT 1 FOR "saml_sign_authn_response"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_teams" ADD "saml_sign_assertions" bit NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_teams" ADD CONSTRAINT "DF_3cc5c2be448344d969685905f1d" DEFAULT 1 FOR "saml_sign_assertions"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_teams" ADD "saml_entry_point" nvarchar(255)`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_teams" ADD "saml_cert" nvarchar(4000)`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_teams" ADD "saml_issuer" nvarchar(255)`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "rvn_teams" ADD "domain" nvarchar(255)`,
     );
     await queryRunner.query(
       `DROP INDEX "IDX_2aaec7693a56e203e22aab677a" ON "rvn_field_definitions"`,
@@ -291,6 +371,14 @@ export class BussinessStructure1696508396234 implements MigrationInterface {
     );
     await queryRunner.query(`DROP TABLE "rvn_organisations"`);
     await queryRunner.query(
+      `DROP INDEX "IDX_eb4435b0a20e30a14eeb869e6a" ON "rvn_pipeline_definitions"`,
+    );
+    await queryRunner.query(`DROP TABLE "rvn_pipeline_definitions"`);
+    await queryRunner.query(
+      `DROP INDEX "IDX_b6fa3f566faad372719dd028e4" ON "rvn_pipeline_stages"`,
+    );
+    await queryRunner.query(`DROP TABLE "rvn_pipeline_stages"`);
+    await queryRunner.query(
       `DROP INDEX "IDX_580829bcf208c4d510ec4c0150" ON "rvn_note_fields"`,
     );
     await queryRunner.query(
@@ -303,9 +391,5 @@ export class BussinessStructure1696508396234 implements MigrationInterface {
       `DROP INDEX "IDX_703e3b0e316225cc92cf81ce88" ON "rvn_note_fields"`,
     );
     await queryRunner.query(`DROP TABLE "rvn_note_fields"`);
-    await queryRunner.query(
-      `DROP INDEX "IDX_aef5ea4009042fc897576b01ee" ON "rvn_affinity_organisations"`,
-    );
-    await queryRunner.query(`DROP TABLE "rvn_affinity_organisations"`);
   }
 }
