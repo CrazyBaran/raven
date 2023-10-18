4bc0c7dc77228f0b76d5bf1ef088c462ee6e6a0f
Mateusz Kłosiński <mkklosin@gmail.com>
2023-10-18 15:16:27 +0200

    RAVE-87 | Updated unit tests

--- libs/rvnc-notes/feature/notes-list/src/lib/rvnc-notes-feature-notes-list/rvnc-notes-feature-notes-list.component.spec.ts
+++ libs/rvnc-notes/feature/notes-list/src/lib/rvnc-notes-feature-notes-list/rvnc-notes-feature-notes-list.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoteStoreFacadeService } from '@app/rvnc-notes/data-access';
+import { provideMockStore } from '@ngrx/store/testing';
 import { RvncNotesFeatureNotesListComponent } from './rvnc-notes-feature-notes-list.component';
 
 describe('RvncNotesFeatureNotesListComponent', () => {
@@ -8,6 +10,7 @@ describe('RvncNotesFeatureNotesListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RvncNotesFeatureNotesListComponent],
+      providers: [NoteStoreFacadeService, provideMockStore({})],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RvncNotesFeatureNotesListComponent);
--- libs/rvnc-notes/feature/notes-list/src/test-setup.ts
+++ libs/rvnc-notes/feature/notes-list/src/test-setup.ts
@@ -1,3 +1,4 @@
+import '@angular/localize/init';
 import 'jest-preset-angular/setup-jest';
 import { TextDecoder, TextEncoder } from 'util';
 
