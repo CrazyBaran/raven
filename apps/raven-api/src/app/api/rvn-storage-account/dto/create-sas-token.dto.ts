1fdc8d250c975d3f8363f810d51333a0c037d122
CrazyBaran <jakbaranowski@gmail.com>
2023-10-18 15:24:34 +0200

    Clean Up

--- apps/raven-api/src/app/api/rvn-storage-account/dto/create-sas-token.dto.ts
+++ apps/raven-api/src/app/api/rvn-storage-account/dto/create-sas-token.dto.ts
@@ -1,5 +1,5 @@
-import {ApiProperty} from "@nestjs/swagger";
-import {IsDefined, IsNotEmpty} from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateSasTokenDto {
   @ApiProperty()
