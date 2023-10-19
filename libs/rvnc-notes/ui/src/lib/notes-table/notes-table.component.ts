220ef6f9ced9e92f861af6b630c488728871b101
Mateusz Kłosiński <mkklosin@gmail.com>
2023-10-19 21:31:02 +0200

    RAVE-89 | Adjusted `Notes` page

--- libs/rvnc-notes/ui/src/lib/notes-table/notes-table.component.ts
+++ libs/rvnc-notes/ui/src/lib/notes-table/notes-table.component.ts
@@ -1,5 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  ViewEncapsulation,
+} from '@angular/core';
 import { NoteData } from '@app/rvns-notes/data-access';
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
 import { GridModule } from '@progress/kendo-angular-grid';
@@ -11,6 +16,7 @@ import { GridModule } from '@progress/kendo-angular-grid';
   templateUrl: './notes-table.component.html',
   styleUrls: ['./notes-table.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class NotesTableComponent {
   @Input({ required: true }) public notes: NoteData[] = [];
