947b1bc0bfa9e5272b8976577391b9d9bea056f6
Szymon Dziedzic <szymon.d@curvestone.io>
2023-10-20 09:47:15 +0200

    RAVE-66 tests fix

--- apps/raven-api/src/app/api/rvn-notes/notes.controller.spec.ts
+++ apps/raven-api/src/app/api/rvn-notes/notes.controller.spec.ts
@@ -2,7 +2,10 @@ import { ParseUUIDPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { EntityManager } from 'typeorm';
+import { FindOrganizationByDomainPipe } from '../../shared/pipes/find-organization-by-domain.pipe';
 import { ParseTemplateWithGroupsAndFieldsPipe } from '../../shared/pipes/parse-template-with-groups-and-fields.pipe';
+import { OrganisationEntity } from '../rvn-opportunities/entities/organisation.entity';
+import { TagEntity } from '../rvn-tags/entities/tag.entity';
 import { NoteEntity } from './entities/note.entity';
 import { NotesController } from './notes.controller';
 import { NotesService } from './notes.service';
@@ -18,8 +21,17 @@ describe('NotesController', () => {
         { provide: EntityManager, useValue: {} },
         { provide: ParseUUIDPipe, useValue: {} },
         { provide: ParseTemplateWithGroupsAndFieldsPipe, useValue: {} },
+        { provide: FindOrganizationByDomainPipe, useValue: {} },
         {
           provide: getRepositoryToken(NoteEntity),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(OrganisationEntity),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(TagEntity),
           useValue: {},
         },
       ],
