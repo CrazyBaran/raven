683258f4771be0b0e7d31404a45f6ba94d780b12
Mateusz Kłosiński <mkklosin@gmail.com>
2023-10-20 07:24:53 +0200

    RAVE-89 | Improved `UI` of `QuickFilters`

--- libs/rvnc-notes/ui/src/lib/quick-filters/quick-filters.component.ts
+++ libs/rvnc-notes/ui/src/lib/quick-filters/quick-filters.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FilterMenuModule } from '@progress/kendo-angular-grid';
+import { QuickFilterData } from './quick-filters.interface';
 
 @Component({
   selector: 'app-quick-filters',
@@ -10,4 +11,46 @@ import { FilterMenuModule } from '@progress/kendo-angular-grid';
   styleUrls: ['./quick-filters.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class QuickFiltersComponent {}
+export class QuickFiltersComponent {
+  public readonly filtersSection1: QuickFilterData[] = [
+    {
+      name: 'Created by me',
+      value: '',
+    },
+    {
+      name: 'I am tagged',
+      value: '',
+    },
+  ];
+
+  public readonly filtersSection2: QuickFilterData[] = [
+    {
+      name: 'All Note Types',
+      value: '',
+    },
+    {
+      name: 'Company Call',
+      value: '',
+    },
+    {
+      name: 'Investor Call',
+      value: '',
+    },
+    {
+      name: 'Market Expert Call',
+      value: '',
+    },
+    {
+      name: 'Customer Reference Call',
+      value: '',
+    },
+    {
+      name: 'Pipeline Call',
+      value: '',
+    },
+    {
+      name: 'Loose Note',
+      value: '',
+    },
+  ];
+}
