2bd86eda85b1f6c34516b1da760d6032eac875a4
Mateusz Kłosiński <mkklosin@gmail.com>
2023-10-20 08:45:44 +0200

    RAVE-89 | Export `NoteDetails` component

--- libs/rvnc-notes/ui/src/index.ts
+++ libs/rvnc-notes/ui/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/note-details/note-details.component';
 export * from './lib/notepad/notepad.component';
 export * from './lib/notes-table/notes-table.component';
 export * from './lib/quick-filters/quick-filters.component';
