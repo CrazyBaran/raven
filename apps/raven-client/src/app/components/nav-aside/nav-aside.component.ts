9d56b4cdd081c848c6feeeefcf095a4f68cbbe15
Mateusz Kłosiński <mkklosin@gmail.com>
2023-10-24 07:45:52 +0200

    RAVE-106 | Quick `sidebar` update implementation

--- apps/raven-client/src/app/components/nav-aside/nav-aside.component.ts
+++ apps/raven-client/src/app/components/nav-aside/nav-aside.component.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
-  inject,
   Input,
   signal,
   WritableSignal,
@@ -11,6 +10,7 @@ import { Router, RouterModule } from '@angular/router';
 import { ShelfStoreFacade } from '@app/rvnc-shelf';
 import { MsalService } from '@azure/msal-angular';
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { TooltipModule } from '@progress/kendo-angular-tooltip';
 import { LoginComponent } from '../../pages/login/login.component';
 import { collapseAnimation } from './nav-aside.animation';
 import { UiNavAsideRoute, UiNavAsideSubRoute } from './nav-aside.interface';
@@ -18,7 +18,13 @@ import { UiNavAsideRoute, UiNavAsideSubRoute } from './nav-aside.interface';
 @Component({
   selector: 'app-nav-aside',
   standalone: true,
-  imports: [CommonModule, ButtonsModule, RouterModule, LoginComponent],
+  imports: [
+    CommonModule,
+    ButtonsModule,
+    RouterModule,
+    LoginComponent,
+    TooltipModule,
+  ],
   templateUrl: './nav-aside.component.html',
   styleUrls: ['./nav-aside.component.scss'],
   animations: [collapseAnimation],
@@ -27,14 +33,34 @@ import { UiNavAsideRoute, UiNavAsideSubRoute } from './nav-aside.interface';
 export class NavAsideComponent {
   @Input() public routes: UiNavAsideRoute[];
 
-  public shelfFacade = inject(ShelfStoreFacade);
-
   public isOpen = signal(false);
+  public visibleUserDetails = signal(false);
   public openRoute: WritableSignal<UiNavAsideRoute | null> = signal(null);
+
+  public readonly userOptions: {
+    name: string;
+    icon: string;
+    action?: () => void;
+  }[] = [
+    {
+      name: 'Support',
+      icon: 'fa-regular fa-circle-info',
+    },
+    {
+      name: 'Settings',
+      icon: 'fa-solid fa-gear',
+    },
+    {
+      name: 'Logout',
+      icon: 'fa-solid fa-arrow-right-to-line',
+      action: (): void => this.handleLogout(),
+    },
+  ];
 
   public constructor(
     private readonly msalService: MsalService,
     private readonly router: Router,
+    private readonly shelfFacade: ShelfStoreFacade,
   ) {}
 
   public get subRoutes(): UiNavAsideSubRoute[] | null {
@@ -49,6 +75,18 @@ export class NavAsideComponent {
     }
 
     return url;
+  }
+
+  public get activeUser(): string {
+    return this.msalService.instance.getActiveAccount()?.name || '';
+  }
+
+  public get userInitials(): string {
+    return this.activeUser
+      .split(' ')
+      .slice(0, 2)
+      .map((char) => char[0])
+      .join('');
   }
 
   public handleToggleSidebar(state?: boolean): void {
@@ -87,6 +125,12 @@ export class NavAsideComponent {
 
   public handleOpenNotepad(): void {
     this.shelfFacade.openNotepad();
+  }
+
+  public handleToggleUserDetails(): void {
+    this.isOpen.set(true);
+
+    this.visibleUserDetails.update((value) => !value);
   }
 
   public handleLogout(): void {
