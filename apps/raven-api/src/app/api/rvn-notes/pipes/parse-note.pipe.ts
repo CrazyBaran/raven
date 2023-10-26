5826c06fe46e620512b05c600bbea841b4b3c340
Szymon Dziedzic <szymon.d@curvestone.io>
2023-10-26 14:23:25 +0200

    not getting note history eagerly

--- apps/raven-api/src/app/api/rvn-notes/pipes/parse-note.pipe.ts
+++ apps/raven-api/src/app/api/rvn-notes/pipes/parse-note.pipe.ts
@@ -1,19 +1,35 @@
-import { AbstractEntityPipe } from '../../../shared/pipes/abstract-entity.pipe';
+import { Repository } from 'typeorm';
+
+import { Injectable, PipeTransform } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { NoteEntity } from '../entities/note.entity';
 
-export class ParseNotePipe extends AbstractEntityPipe<NoteEntity> {
-  public readonly entityClass = NoteEntity;
-  public readonly resource = 'note';
-  public readonly relations = [
-    'createdBy',
-    'updatedBy',
-    'deletedBy',
-    'tags',
-    'template',
-    'noteTabs',
-    'noteTabs.noteFieldGroups',
-    'noteTabs.noteFieldGroups.noteFields',
-    'noteFieldGroups',
-    'noteFieldGroups.noteFields',
-  ];
+@Injectable()
+export class ParseNotePipe
+  implements PipeTransform<string, Promise<NoteEntity>>
+{
+  @InjectRepository(NoteEntity)
+  protected noteRepository: Repository<NoteEntity>;
+
+  public async transform(id: string): Promise<NoteEntity> {
+    const result = await this.noteRepository.findOne({
+      where: { id },
+      relations: [
+        'createdBy',
+        'updatedBy',
+        'deletedBy',
+        'tags',
+        'template',
+        'noteTabs',
+        'noteTabs.noteFieldGroups',
+        'noteTabs.noteFieldGroups.noteFields',
+        'noteFieldGroups',
+        'noteFieldGroups.noteFields',
+      ],
+    });
+    if (!result) {
+      throw new Error(`Note with id ${id} not found`);
+    }
+    return result;
+  }
 }
