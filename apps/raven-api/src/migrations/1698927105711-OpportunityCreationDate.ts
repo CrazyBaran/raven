8fdb249a66158075178eb33ffbc5f8fe71697427
Szymon Dziedzic <szymon.d@curvestone.io>
2023-11-09 12:39:09 +0100

    migration fix (#184)

--- apps/raven-api/src/migrations/1698927105711-OpportunityCreationDate.ts
+++ apps/raven-api/src/migrations/1698927105711-OpportunityCreationDate.ts
@@ -7,12 +7,6 @@ export class OpportunityCreationDate1698927105711
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "rvn_cca_token_caches" DROP CONSTRAINT "DF_6716c3263ea593141e2adcb823e"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "rvn_cca_token_caches" DROP COLUMN "is_encrypted"`,
-    );
-    await queryRunner.query(
       `ALTER TABLE "rvn_opportunities" ADD "created_at" datetime2 NOT NULL CONSTRAINT "DF_0ab62cb9414d4c852fdf0e6d74a" DEFAULT getdate()`,
     );
   }
@@ -23,12 +17,6 @@ export class OpportunityCreationDate1698927105711
     );
     await queryRunner.query(
       `ALTER TABLE "rvn_opportunities" DROP COLUMN "created_at"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "rvn_cca_token_caches" ADD "is_encrypted" bit NOT NULL`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "rvn_cca_token_caches" ADD CONSTRAINT "DF_6716c3263ea593141e2adcb823e" DEFAULT 0 FOR "is_encrypted"`,
     );
   }
 }
