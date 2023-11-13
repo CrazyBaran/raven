979807f471bc74cb83a109eea90080c5854a8a80
FilipMakowski <102666802+FilipMakowski@users.noreply.github.com>
2023-11-13 17:49:35 +0100

    Update environment.prod.ts to reflect on staging env.

--- apps/raven-client/src/environments/environment.prod.ts
+++ apps/raven-client/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://as-wa-mub-raven-prod.azurewebsites.net',
-  adClientId: '???',
-  adAuthority: '???',
-  adRedirectUri: '???',
-  adPostLogoutRedirectUri: '???',
+  apiUrl: 'https://raven-staging-api.mubadalacapital.ae',
+  adClientId: 'fc392811-e450-47c1-8928-0ac8def0bb9e',
+  adAuthority: 'https://login.microsoftonline.com/c22477b0-4838-4110-8b5c-72e85395fab5',
+  adRedirectUri: 'https://raven-staging.mubadalacapital.ae',
+  adPostLogoutRedirectUri: 'https://raven-staging.mubadalacapital.ae/login',
 };
