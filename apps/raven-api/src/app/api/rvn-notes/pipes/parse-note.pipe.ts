3c63787fc27bdd329415b0518b962a2d6a58477a
Szymon Dziedzic <szymon.d@curvestone.io>
2023-11-20 12:19:52 +0100

    update note fix

--- apps/raven-api/src/app/api/rvn-notes/pipes/parse-note.pipe.ts
+++ apps/raven-api/src/app/api/rvn-notes/pipes/parse-note.pipe.ts
@@ -1,20 +1,45 @@
-import { AbstractEntityPipe } from '../../../shared/pipes/abstract-entity.pipe';
+import { PipeTransform } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TemplateEntity } from '../../rvn-templates/entities/template.entity';
 import { NoteEntity } from '../entities/note.entity';
 
-export class ParseNotePipe extends AbstractEntityPipe<NoteEntity> {
-  public readonly entityClass = NoteEntity;
-  public readonly resource = 'note';
-  public readonly relations = [
-    'createdBy',
-    'updatedBy',
-    'deletedBy',
-    'tags',
-    'complexTags',
-    'template',
-    'noteTabs',
-    'noteTabs.noteFieldGroups',
-    'noteTabs.noteFieldGroups.noteFields',
-    'noteFieldGroups',
-    'noteFieldGroups.noteFields',
-  ];
+export class ParseNotePipe
+  implements PipeTransform<string, Promise<NoteEntity>>
+{
+  @InjectRepository(NoteEntity)
+  protected noteRepository: Repository<NoteEntity>;
+  @InjectRepository(TemplateEntity)
+  protected templateRepository: Repository<TemplateEntity>;
+
+  public async transform(id: string): Promise<NoteEntity> {
+    const note = await this.noteRepository.findOne({
+      where: { id },
+      relations: [
+        'createdBy',
+        'updatedBy',
+        'deletedBy',
+        'tags',
+        'complexTags',
+        'noteTabs',
+        'noteTabs.noteFieldGroups',
+        'noteTabs.noteFieldGroups.noteFields',
+        'noteFieldGroups',
+        'noteFieldGroups.noteFields',
+      ],
+    });
+
+    if (!note) {
+      throw new Error(`Note with id ${id} not found`);
+    }
+
+    // handling template separately as typeorm nested relations is pain
+    const template = await this.templateRepository.findOne({
+      where: { id: note.templateId },
+    });
+    if (template) {
+      note.template = template;
+    }
+    return note;
+  }
 }
