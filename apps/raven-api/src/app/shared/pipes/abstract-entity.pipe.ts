679b631c11fc5a4dd390fd87fb18e5c02426596c
Szymon Dziedzic <szymon.d@curvestone.io>
2023-12-05 08:33:24 +0100

    restore abstract pipe

--- apps/raven-api/src/app/shared/pipes/abstract-entity.pipe.ts
+++ apps/raven-api/src/app/shared/pipes/abstract-entity.pipe.ts
@@ -53,9 +53,6 @@ export abstract class AbstractEntityPipe<E>
         const missingIds = id.filter(
           (id) => !entities.some((entity) => entity[this.entityField] === id),
         );
-        if (this.optional) {
-          return null;
-        }
         throw new NotFoundException(
           `Unable to find "${this.resource}" with ids: "${missingIds.join(
             ', ',
@@ -63,6 +60,9 @@ export abstract class AbstractEntityPipe<E>
         );
       }
       return entities;
+    }
+    if (this.optional) {
+      return null;
     }
     throw new NotFoundException(
       `"${metadata.data}" should not be null or undefined`,
