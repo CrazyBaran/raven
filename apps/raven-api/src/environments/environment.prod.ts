5ef6cccc8b01e5ad33061839de46582fae722cbf
FilipMakowski <102666802+FilipMakowski@users.noreply.github.com>
2023-12-08 11:25:53 +0100

    RAVE-413: Reverted automatic migrations. (#321)

--- apps/raven-api/src/environments/environment.prod.ts
+++ apps/raven-api/src/environments/environment.prod.ts
@@ -97,9 +97,6 @@ export const environment = {
       subscribers: [],
       autoLoadEntities: true,
       namingStrategy: new SnakeNamingStrategy(),
-      migrations: ['./apps/raven-api/src/migrations/*.ts'],
-      migrationsTableName: 'rvn_migrations',
-      migrationsRun: true,
       options: {
         enableArithAbort: true,
         useUTC: true,
