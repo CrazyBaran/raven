7fcfed694186b0020d696e919d9c83f2a2dc7599
Szymon Dziedzic <szymon.d@curvestone.io>
2023-12-11 08:48:28 +0100

    RAVE-190 wip

---
+++ apps/raven-api/src/app/api/rvn-templates/entities/field-configuration.entity.ts
@@ -0,0 +1,37 @@
+import {
+  AfterInsert,
+  AfterLoad,
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  PrimaryGeneratedColumn,
+  RelationId,
+} from 'typeorm';
+import { FieldDefinitionEntity } from './field-definition.entity';
+
+@Entity({ name: 'field_configuration' })
+@Index(['id'], { unique: true })
+export class FieldConfigurationEntity {
+  @PrimaryGeneratedColumn('uuid')
+  public id: string;
+
+  @Column({ length: 50 })
+  public key: string;
+
+  @Column()
+  public value: string;
+
+  @JoinColumn({ name: 'field_id' })
+  public fieldDefinition: FieldDefinitionEntity;
+
+  @Column()
+  @RelationId((fc: FieldConfigurationEntity) => fc.fieldDefinition)
+  public fieldDefinitionId: string;
+
+  @AfterInsert()
+  @AfterLoad()
+  public lifecycleUuidLowerCase(): void {
+    this.id = this.id.toLowerCase();
+  }
+}
