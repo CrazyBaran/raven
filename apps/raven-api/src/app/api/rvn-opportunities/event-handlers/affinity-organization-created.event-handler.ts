cbb68d8695484857fe06023befb51fe46f694981
Szymon Dziedzic <szymon.d@curvestone.io>
2023-12-12 14:25:07 +0100

    create opportunity if organisation exists

--- apps/raven-api/src/app/api/rvn-opportunities/event-handlers/affinity-organization-created.event-handler.ts
+++ apps/raven-api/src/app/api/rvn-opportunities/event-handlers/affinity-organization-created.event-handler.ts
@@ -1,12 +1,14 @@
 import { AffinityOrganizationCreatedEvent } from '@app/rvns-affinity-integration';
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
+import { AffinityCacheService } from '../../rvn-affinity-integration/cache/affinity-cache.service';
 import { OrganisationService } from '../organisation.service';
 
 @Injectable()
 export class AffinityOrganisationCreatedEventHandler {
   public constructor(
     private readonly organisationService: OrganisationService,
+    private readonly affinityCacheService: AffinityCacheService,
   ) {}
 
   @OnEvent('affinity-organization-created')
@@ -15,7 +17,19 @@ export class AffinityOrganisationCreatedEventHandler {
   ): Promise<void> {
     if (!event.domains || event.domains.length === 0) return;
 
-    if (await this.organisationService.exists(event.domains)) {
+    const organisationEntity =
+      await this.organisationService.getExistingByDomains(event.domains);
+    if (organisationEntity) {
+      if (!event.createOpportunity) {
+        return;
+      }
+      const organizationStageDtos =
+        await this.affinityCacheService.getByDomains(event.domains);
+
+      await this.organisationService.createOpportunityForOrganisation(
+        organisationEntity,
+        organizationStageDtos[0].stage?.text || null,
+      );
       return;
     }
 
