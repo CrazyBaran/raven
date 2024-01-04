d05bbbbb10235160cf32428d5f02570c763d6472
Szymon Dziedzic <szymon.d@curvestone.io>
2024-01-04 08:58:38 +0100

    RAVE-506 remove debug

--- apps/raven-api/src/app/api/rvn-notes/notes.service.ts
+++ apps/raven-api/src/app/api/rvn-notes/notes.service.ts
@@ -583,24 +583,12 @@ export class NotesService {
         new Date().getTime() - start,
       );
 
-      console.log({
-        notevals: JSON.stringify(
-          savedNewNoteVersion.noteTabs.flatMap((nt) =>
-            nt.noteFieldGroups.flatMap((nfg) =>
-              nfg.noteFields.map((f) => ({ name: f.name, value: f.value })),
-            ),
-          ),
-        ),
-        templateType,
-      });
-
       if (templateType === TemplateTypeEnum.Workflow) {
         start = new Date().getTime();
         const opportunity = await this.opportunityRepository.findOne({
           where: { noteId: noteEntity.id },
           relations: ['note'],
         });
-        console.log({ opportunity, noteEntity });
         if (opportunity) {
           if (
             opportunity.note &&
