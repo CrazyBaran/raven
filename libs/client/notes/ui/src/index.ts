d03fbb7ea5ba18c37e14c8ec695f7935654d2b23
jakNatonek <jakub.natonek@houseofangular.io>
2024-01-04 13:25:22 +0100

    removed unused component (quick-filters)

--- libs/client/notes/ui/src/index.ts
+++ libs/client/notes/ui/src/index.ts
@@ -4,5 +4,4 @@ export * from './lib/notepad-form/notepad-form.component';
 export * from './lib/notepad-template/notepad-template.component';
 export * from './lib/notepad/notepad.component';
 export * from './lib/notes-table/notes-table.component';
-export * from './lib/quick-filters/quick-filters.component';
 export * from './lib/scroll-tab/scroll-tab.component';
--- libs/client/notes/ui/src/lib/quick-filters/quick-filters.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface QuickFilterData {
-  name: string;
-  value: string;
-}
