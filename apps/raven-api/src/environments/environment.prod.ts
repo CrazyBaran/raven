1753ff848e27370b4d208500821c847bc74b3f85
FilipMakowski <102666802+FilipMakowski@users.noreply.github.com>
2024-01-05 13:25:54 +0100

    RAVE-490: Changing auth mechanism for DWH on DEV. (#382)
    * RAVE-490: An attempt to connect to data warehouse with a managed identity on DEV.
    
    * RAVE-490: Updated TypeORM version
    
    * RAVE-490: Testing out other way to authenticate.

--- apps/raven-api/src/environments/environment.prod.ts
+++ apps/raven-api/src/environments/environment.prod.ts
@@ -117,7 +117,7 @@ export const environment = {
       port: env.get('DWH_PORT').default(1433).asPortNumber(),
       database: env.get('DWH_DATABASE').asString(),
       authentication: {
-        type: 'azure-active-directory-default',
+        type: 'azure-active-directory-msi-app-service',
         options: {
           clientId: env.get('AD_CLIENT_ID').asString(),
         },
