abffd81c79b2216c193c8f01e32855a65e79b809
FilipMakowski <102666802+FilipMakowski@users.noreply.github.com>
2024-01-08 09:33:20 +0100

    RAVE-490: Attempt to use access token approach. (#391)

--- apps/raven-api/src/app/api/rvn-data-warehouse/data-warehouse.module.ts
+++ apps/raven-api/src/app/api/rvn-data-warehouse/data-warehouse.module.ts
@@ -1,24 +1,47 @@
+import { AccessToken } from '@azure/core-http';
+import { DefaultAzureCredential } from '@azure/identity';
 import { DynamicModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { SqlServerConnectionCredentialsAuthenticationOptions } from 'typeorm/driver/sqlserver/SqlServerConnectionCredentialsOptions';
 import { SqlServerConnectionOptions } from 'typeorm/driver/sqlserver/SqlServerConnectionOptions';
 import { environment } from '../../../environments/environment';
-
 @Module({})
 export class DataWarehouseModule {
   public static async forRootAsync(): Promise<DynamicModule> {
     if (environment.features.dataWareHouse === false) {
+      console.log('DataWarehouse connection stopped: feature disabled');
       return {
         module: DataWarehouseModule,
       };
     }
+    const defaultCredential = new DefaultAzureCredential();
+    let accessToken: AccessToken;
+    defaultCredential
+      .getToken('https://database.windows.net/')
+      .then((token) => {
+        accessToken = token;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
+    if (!accessToken) {
+      console.log(
+        'DataWarehouse module initialisation stopped: no access token',
+      );
+      return {
+        module: DataWarehouseModule,
+      };
+    }
     const dataWarehouseConfig = environment.database.dataWarehouse;
     const alteredConfig = {
       ...dataWarehouseConfig,
       authentication: {
-        type: 'azure-active-directory-default',
+        type: 'azure-active-directory-access-token',
+        options: {
+          token: accessToken.token,
+        },
       } as SqlServerConnectionCredentialsAuthenticationOptions,
     } as SqlServerConnectionOptions;
 
@@ -41,9 +64,12 @@ export class DataWarehouseModule {
       await dataWarehouseDataSource.destroy();
       return {
         module: DataWarehouseModule,
-        imports: [TypeOrmModule.forRoot(environment.database.dataWarehouse)],
+        imports: [TypeOrmModule.forRoot(alteredConfig)],
       };
     } else {
+      console.log(
+        'DataWarehouse module initialisation stopped: connection failed',
+      );
       return {
         module: DataWarehouseModule,
       };
