481ee839d521fdd82b04463a1542c4d7f96497ac
Szymon Dziedzic <szymon.d@curvestone.io>
2024-01-17 09:57:27 +0100

    RAVE-499 remove dev code

--- apps/raven-api/src/app/api/rvn-pipeline/pipeline.controller.ts
+++ apps/raven-api/src/app/api/rvn-pipeline/pipeline.controller.ts
@@ -100,7 +100,7 @@ export class PipelineController {
   @ApiOperation({ description: 'Create pipeline groups' })
   @ApiResponse(GenericResponseSchema())
   @ApiParam({ name: 'pipelineId', type: 'string' })
-  @Roles(RoleEnum.User, RoleEnum.SuperAdmin) // TODO remove
+  @Roles(RoleEnum.SuperAdmin)
   @Post(':pipelineId/groups')
   public async createPipelineGroups(
     @Param('pipelineId', ParseUUIDPipe, ParsePipelineWithStagesPipe)
