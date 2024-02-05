52319896f686bdd4506fffe70b8c08c8d693b2a7
jakNatonek <jakub.natonek@houseofangular.io>
2024-02-05 11:05:59 +0100

    fix build

--- libs/client/organisations/feature/organisations-table-v2/src/test-setup.ts
+++ libs/client/organisations/feature/organisations-table-v2/src/test-setup.ts
@@ -1,8 +1,23 @@
-// @ts-expect-error https://thymikee.github.io/jest-preset-angular/docs/getting-started/test-environment
-globalThis.ngJest = {
-  testEnvironmentOptions: {
-    errorOnUnknownElements: true,
-    errorOnUnknownProperties: true,
+/* eslint-disable */
+export default {
+  displayName: 'client-organisations-feature-organisations-table',
+  preset: '../../../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory:
+    '../../../../../coverage/libs/client/organisations/feature/organisations-table',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
   },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
 };
-import 'jest-preset-angular/setup-jest';
