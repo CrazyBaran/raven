e2a1224ddb18fae15a724ca137b96b2e73ed6a03
CrazyBaran <jakub@curvestone.io>
2024-02-19 10:05:16 +0100

    Small clean up

--- apps/raven-api/src/app/api/rvn-affinity-integration/queues/affinity-data-watchdog.processor.ts
+++ apps/raven-api/src/app/api/rvn-affinity-integration/queues/affinity-data-watchdog.processor.ts
@@ -2,12 +2,11 @@ import { AbstractSimpleQueueProcessor } from '@app/rvns-bull';
 import { RavenLogger } from '../../rvn-logger/raven.logger';
 import { JobPro } from '@taskforcesh/bullmq-pro';
 import { Processor } from '@taskforcesh/nestjs-bullmq-pro';
-import { AFFINITY_CACHE, AFFINITY_DATA_WATCHDOG, AFFINITY_FIELDS_CACHE, AFFINITY_QUEUE } from '../affinity.const';
+import { AFFINITY_CACHE, AFFINITY_DATA_WATCHDOG, AFFINITY_FIELDS_CACHE } from '../affinity.const';
 import { AffinityProducer } from './affinity.producer';
 import { Inject } from '@nestjs/common';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
-import { AffinityCacheService } from '../cache/affinity-cache.service';
 import { RedisStore } from 'cache-manager-ioredis-yet';
 
 export interface AffinityDataWatchdogJobData {
