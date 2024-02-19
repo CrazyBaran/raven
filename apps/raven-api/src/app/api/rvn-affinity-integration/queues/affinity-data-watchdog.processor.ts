15fe6590d63490b1ccf42d1565f7d26a985ceb6e
CrazyBaran <jakub@curvestone.io>
2024-02-19 10:03:46 +0100

    Lack of await

--- apps/raven-api/src/app/api/rvn-affinity-integration/queues/affinity-data-watchdog.processor.ts
+++ apps/raven-api/src/app/api/rvn-affinity-integration/queues/affinity-data-watchdog.processor.ts
@@ -16,13 +16,13 @@ export interface AffinityDataWatchdogJobData {
 @Processor(AFFINITY_DATA_WATCHDOG, {
   concurrency: 1,
   group: { concurrency: 1 },
-  removeOnComplete: { count: 10 },
+  removeOnComplete: { count: 10 }
 })
 export class AffinityDataWatchdogProcessor extends AbstractSimpleQueueProcessor<AffinityDataWatchdogJobData> {
   public constructor(
     private readonly affinityProducer: AffinityProducer,
     @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
-    public readonly logger: RavenLogger,) {
+    public readonly logger: RavenLogger) {
     logger.setContext(AffinityDataWatchdogProcessor.name);
     super(logger);
   }
@@ -34,9 +34,9 @@ export class AffinityDataWatchdogProcessor extends AbstractSimpleQueueProcessor<
   public async process(job: JobPro): Promise<boolean> {
     const listFieldsCount = await this.store.client.hlen(AFFINITY_FIELDS_CACHE);
     const affinityCacheCount = await this.store.client.hlen(AFFINITY_CACHE);
-    if(listFieldsCount === 0 || affinityCacheCount === 0){
+    if (listFieldsCount === 0 || affinityCacheCount === 0) {
       this.logger.warn('Affinity data watchdog didnt find any data. Regenerating data.');
-      this.affinityProducer.enqueueRegenerateAffinityData();
+      await this.affinityProducer.enqueueRegenerateAffinityData();
     }
 
     return true;
