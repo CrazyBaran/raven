1b3db5046435a75c8a5b3c6f1494e2220eac1b5a
CrazyBaran <jakub@curvestone.io>
2024-02-21 11:48:17 +0100

    Separate tracing settings to different file like was written in Good Practices.

---
+++ apps/raven-api/src/tracing.ts
@@ -0,0 +1,58 @@
+import {
+  AzureMonitorOpenTelemetryOptions,
+  useAzureMonitor,
+} from '@azure/monitor-opentelemetry';
+import { Resource } from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import * as env from 'env-var';
+
+let customResource: Resource = new Resource({});
+if (
+  env.get('LOCAL_INSTANCE_NAME').default('undefined').asString() !== 'undefined'
+) {
+  customResource = customResource.merge(
+    new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: 'as-wa-mc-raven-dev',
+      [SemanticResourceAttributes.SERVICE_INSTANCE_ID]: env
+        .get('LOCAL_INSTANCE_NAME')
+        .asString(),
+    }),
+  );
+}
+
+// Create a new AzureMonitorOpenTelemetryOptions object.
+const options: AzureMonitorOpenTelemetryOptions = {
+  instrumentationOptions: {
+    azureSdk: { enabled: true },
+  },
+  azureMonitorExporterOptions: {
+    connectionString: env
+      .get('APPLICATIONINSIGHTS_CONNECTION_STRING')
+      .asString(),
+  },
+  resource: customResource,
+  samplingRatio: 1,
+};
+
+// Enable Azure Monitor integration using the useAzureMonitor function and the AzureMonitorOpenTelemetryOptions object.
+useAzureMonitor(options);
+
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { IORedisInstrumentation } from '@opentelemetry/instrumentation-ioredis';
+import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
+import { BullMQProInstrumentation } from 'opentelemetry-instrumentation-bullmqpro';
+import { TypeormInstrumentation } from 'opentelemetry-instrumentation-typeorm';
+
+registerInstrumentations({
+  // List of instrumentations to register
+  instrumentations: [
+    new NestInstrumentation(),
+    new HttpInstrumentation(),
+    new ExpressInstrumentation(),
+    new IORedisInstrumentation(), // for ioredis instrumentation
+    new TypeormInstrumentation(), // for typeorm instrumentation
+    new BullMQProInstrumentation(),
+  ],
+});
