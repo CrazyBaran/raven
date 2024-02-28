fd24196a8e24a12f8b0912a27fc26eea305c4a05
FilipMakowski <102666802+FilipMakowski@users.noreply.github.com>
2024-02-28 18:40:26 +0100

    Update environment.prod.ts to have shortlistsFeature as true

--- apps/raven-client/src/environments/environment.prod.ts
+++ apps/raven-client/src/environments/environment.prod.ts
@@ -17,5 +17,5 @@ export const environment: Environment = {
     'b!5FlQMrtGjUmiYJj3y40t--qau7WP-uRBqy0COkR9--xujk9FQtU_ToCpw10tbcQJ',
   sharepointRootDirectoryId: '01TMG4F3ZIOUZNA6XIYNCLU6GK7VG3EYMV',
   websocketUrl: 'https://app-raven-prod-uks.azurewebsites.net/',
-  shortlistsFeature: false,
+  shortlistsFeature: true,
 };
