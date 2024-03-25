f6cd1de5e2b15bf3861692c0c9f85e141db1043d
FilipMakowski <102666802+FilipMakowski@users.noreply.github.com>
2024-03-25 13:54:00 +0100

    FIX: cleaning domain in DWH cache from now on. (#629)

--- apps/raven-api/src/app/api/rvn-data-warehouse/v2/mappers/company.v2.mapper.ts
+++ apps/raven-api/src/app/api/rvn-data-warehouse/v2/mappers/company.v2.mapper.ts
@@ -1,5 +1,6 @@
 import { CompanyDto, exposedCompanyData } from '@app/shared/data-warehouse';
 import { Injectable } from '@nestjs/common';
+import { DomainResolver } from '../../../rvn-utils/domain.resolver';
 import { MapperBase } from '../../interfaces/mapper.base';
 import { DataWarehouseParser } from '../../utils/data-warehouse.parser';
 import { CompanyV2DwhEntity } from '../entities/company.v2.dwh.entity';
@@ -9,14 +10,14 @@ export class CompanyV2Mapper extends MapperBase<
   CompanyV2DwhEntity,
   CompanyDto
 > {
-  public constructor() {
+  public constructor(private readonly domainResolver: DomainResolver) {
     super();
     this.exposedData = exposedCompanyData;
   }
 
   protected buildObject(entity: CompanyV2DwhEntity): Partial<CompanyDto> {
     return {
-      domain: entity.domain,
+      domain: this.domainResolver.cleanDomain(entity.domain),
       name: entity.name,
       description: entity.description,
       mcvLeadScore: entity.mcvLeadScore,
