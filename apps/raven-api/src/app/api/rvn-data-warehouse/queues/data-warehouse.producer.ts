97f340fb7b08d5c13304d4c1894cb570a0670f97
FilipMakowski <102666802+FilipMakowski@users.noreply.github.com>
2024-04-12 11:49:53 +0200

    Fix/dwh queue regen (#700)
    * FIX: Fixed how we queue regeneration job.
    
    * FIX: Small adjustments.

--- apps/raven-api/src/app/api/rvn-data-warehouse/queues/data-warehouse.producer.ts
+++ apps/raven-api/src/app/api/rvn-data-warehouse/queues/data-warehouse.producer.ts
@@ -1,15 +1,40 @@
 import { InjectQueue } from '@nestjs/bullmq';
 import { Injectable } from '@nestjs/common';
 import { Queue } from '@taskforcesh/bullmq-pro';
+import { RavenLogger } from '../../rvn-logger/raven.logger';
 import { DWH_QUEUE } from '../data-warehouse.const';
 
 @Injectable()
 export class DataWarehouseProducer {
   public constructor(
     @InjectQueue(DWH_QUEUE.NAME) private readonly queue: Queue,
-  ) {}
+    private readonly logger: RavenLogger,
+  ) {
+    this.logger.setContext(DataWarehouseProducer.name);
+  }
 
   public async enqueueRegenerateDataWarehouse(): Promise<void> {
+    const existingJob = await this.queue.getJob(DWH_QUEUE.JOBS.REGENERATE);
+
+    const state = existingJob ? await existingJob.getState() : null;
+    if (state && state !== 'completed' && state !== 'failed') {
+      this.logger.warn(
+        'Cache regeneration job is still active when queueing another one',
+      );
+      return;
+    }
+
+    if (!(await this.cleanProxyRegenerationJobs())) {
+      this.logger.warn(
+        'Proxy regeneration job is still active when queueing cache regeneration job',
+      );
+      return;
+    }
+
+    if (existingJob) {
+      await existingJob.remove();
+    }
+
     await this.queue.add(
       DWH_QUEUE.JOBS.REGENERATE,
       {},
@@ -42,5 +67,23 @@ export class DataWarehouseProducer {
         jobId: `${DWH_QUEUE.JOBS.REGENERATE_PROXY}-${skip}-${take}`,
       },
     );
+  }
+
+  private async cleanProxyRegenerationJobs(): Promise<boolean> {
+    const jobs = await this.queue.getJobs();
+    const proxyJobs = jobs.filter((job) =>
+      job.id.startsWith(DWH_QUEUE.JOBS.REGENERATE_PROXY),
+    );
+
+    // if any of the proxy jobs is active, return false
+    const activeProxyJob = proxyJobs.find((job) => job.isActive());
+    if (activeProxyJob) {
+      return false;
+    }
+
+    // remove all proxy jobs
+    await Promise.all(proxyJobs.map((job) => job.remove()));
+
+    return true;
   }
 }
